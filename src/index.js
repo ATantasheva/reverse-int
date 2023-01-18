147ba7d1411403ac9238ce58a098d499c31dd26c
ATantasheva <a4ypaxoid@yandex.ru>
2023-01-18 11:26:27 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   let arr1 = n.toString();
+   let arr = arr1.split('');
+  // console.log(arr);
+  let arr2 = arr.reverse();
+  let arr3 = arr2.join('');
+  let num = parseInt(arr3);
+ // console.log(num);
+return num;
 }
